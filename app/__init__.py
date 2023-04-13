fbcbeb95887729991914c412625b2f689bdd36a6
LIKKIC <likhi0802@gmail.com>
2023-04-13 12:51:26 +0530

    message

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import psycopg2
 
 app = Flask(__name__)
 postgres = psycopg2.connect(
-        host="@dpg-cgrq5c1mbg5e4khqpd90-a.oregon-postgres.render.com",
+        host="dpg-cgrq5c1mbg5e4khqpd90-a.oregon-postgres.render.com",
         database="todoapp_57z4",
         user="root",
         password="IvjrLrQjQ46plRwCrlt5mZWkPaUxipg5")
